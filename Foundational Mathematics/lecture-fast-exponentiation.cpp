77586925632f98e29a0bf9afa01bbd4aea071cf2
talhaarshad7 <talhaarshad7860@gmail.com>
2021-09-27 23:48:02 +0530

    update

---
+++ Foundational Mathematics/lecture-fast-exponentiation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_exponentiation(int a, int b, int mod = 1e9 + 7)
+{
+    int ans = 1;
+    while (b)
+    {
+        if (b % 2)
+            ans = ans * a % mod;
+        a = a * a % mod;
+        b /= 2;
+    }
+    return ans;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << binary_exponentiation(a, b, 10e9 + 1) << endl;
+}
