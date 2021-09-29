6ea93e3a6ada996eb91f271aa645f3297eac807d
talhaarshad7 <talhaarshad7860@gmail.com>
2021-09-29 17:14:46 +0530

    adding superminimum sum

--- STL/superminimumsum.cpp
+++ STL/superminimumsum.cpp
@@ -1,53 +1,3 @@
-// Description
-
-// You have given an array A of size N. A1, A2, ..., AN are the elements of the array. All elements in the array A are distinct.
-// Find the sum of the minimum of all subarrays possible of array A.
-
-// Input Format
-
-// The first line contains T, the number of test cases.
-// The first line of each test case contains N, the number of integers in an array A.
-// The second line of each test case contains N space-separated integers A1, A2, ..., AN.
-
-// Output Format
-
-// For each test case, print a single number representing the sum of the minimum of all subarrays of A.
-
-// Constraints
-
-// 1 ≤ T ≤ 100000
-// 1 ≤ N ≤ 100000
-// 1 ≤ Ai ≤ 106
-// Sum of N over all test cases ≤ 5 * 105.
-// It is guaranteed that all elements in an array A are distinct.
-
-// Sample Input 1
-// 5
-// 3
-// 1 2 3
-// 2
-// 1 5
-// 4
-// 2 3 4 10
-// 8
-// 1000000 1000001 1000002 1000003 1000004 1000005 1000006 1000007
-// 3
-// 3 1 2
-// Sample Output 1
-//  Copy
-
-// 10
-// 7
-// 35
-// 36000084
-// 9
-// Note
-
-// Explanation 1:
-// Min{1} + Min{1, 2} + Min{1, 2, 3} + Min{2} + Min{2, 3} + Min{3} = 1 + 1 + 1 + 2 + 2 + 3 = 10
-
-// Explanation 2:
-// Min{1} + Min{1, 5} + Min{5} = 1 + 1 + 5 = 7
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
@@ -84,7 +34,7 @@ typedef vector<vector<ll>> vv64;
 typedef vector<vector<p64>> vvp64;
 typedef vector<p64> vp64;
 typedef vector<p32> vp32;
-ll MOD = 998244353;
+ll MOD = 1e9 + 7;
 double eps = 1e-12;
 #define forn(i, e) for (ll i = 0; i < e; i++)
 #define forsn(i, s, e) for (ll i = s; i < e; i++)
@@ -106,6 +56,40 @@ double eps = 1e-12;
 
 void solve()
 {
+    ll n;
+    cin >> n;
+    ll arr[n];
+    for (ll i = 0; i < n; i++)
+        cin >> arr[i];
+    ll left[n];
+    ll right[n];
+    stack<pair<ll, ll>> s1, s2;
+    for (ll i = 0; i < n; i++)
+    {
+        ll count = 1;
+        while (!s1.empty() && s1.top().first > arr[i])
+        {
+            count += s1.top().second;
+            s1.pop();
+        }
+        left[i] = count;
+        s1.push({arr[i], count});
+    }
+    for (ll i = n - 1; i >= 0; i--)
+    {
+        ll count = 1;
+        while (!s2.empty() && s2.top().first >= arr[i])
+        {
+            count += s2.top().second;
+            s2.pop();
+        }
+        right[i] = count;
+        s2.push({arr[i], count});
+    }
+    ll sum = 0;
+    for (ll i = 0; i < n; i++)
+        sum += arr[i] * left[i] * right[i];
+    cout << sum << endl;
 }
 int main()
 {
