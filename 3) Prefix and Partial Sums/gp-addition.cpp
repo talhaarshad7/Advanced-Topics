c70c597b91c8d258f87bf94bb35bbe82c9aa82c1
talhaarshad7 <talhaarshad7860@gmail.com>
2021-11-01 23:25:21 +0530

    added rectangel sum

---
+++ 3) Prefix and Partial Sums/gp-addition.cpp
@@ -0,0 +1,154 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+ll mod = 1e9 + 7;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+inline void add(int &a, int b)
+{
+    a += b;
+    if (a >= mod)
+        a -= mod;
+}
+
+inline void sub(int &a, int b)
+{
+    a -= b;
+    if (a < 0)
+        a += mod;
+}
+
+inline int mul(int a, int b)
+{
+    return (int)((long long)a * b % mod);
+}
+
+inline int power(int a, long long b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = mul(res, a);
+        }
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+
+inline int inv(int a)
+{
+    a %= mod;
+    if (a < 0)
+        a += mod;
+    int b = mod, u = 0, v = 1;
+    while (a)
+    {
+        int t = b / a;
+        b -= t * a;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    assert(b == 1);
+    if (u < 0)
+        u += mod;
+    return u;
+}
+
+const int N = 200010;
+
+int fact[N];
+
+void pre()
+{
+    fact[0] = 1;
+    for (int i = 1; i < N; i++)
+        fact[i] = mul(fact[i - 1], i);
+    return;
+}
+
+void solve()
+{
+    int n, q, k;
+    cin >> n >> q >> k;
+    int A[n + 2];
+    memeset(A, 0, sizeof(A));
+    while (q--)
+    {
+        int a, l, r;
+        cin >> a >> l >> r;
+        add(A[l], a);
+        sub(A[r + 1], mul(a, power(k, (r - l + 1))));
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        A[i] = A[i - 1] + A[i];
+        cout << A[i] << endl;
+    }
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        //cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
