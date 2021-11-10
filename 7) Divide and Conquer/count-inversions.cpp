b49a777a3ada101a892435535636af9e62e58766
talhaarshad7 <talhaarshad7860@gmail.com>
2021-11-10 18:59:06 +0530

    added count inversions

---
+++ 7) Divide and Conquer/count-inversions.cpp
@@ -0,0 +1,109 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+ll mod = 1e9 + 7;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+int answer(vector<int> a, int l, int r)
+{
+    if (l == r)
+        return 0;
+    if (l > r)
+        return 0;
+    if (r - l == 1)
+    {
+        if (a[r] < a[l])
+        {
+            swap(a[r], a[l]);
+            return 1;
+        }
+        return 0;
+    }
+    int mid = (l + r) / 2;
+    int ptr = mid + 1;
+    int ans = answer(a, l, mid) + answer(a, mid + 1, r);
+    for (int i = l; i <= mid; i++)
+    {
+        while (ptr <= r && a[ptr] < a[i])
+            ptr++;
+        ans += (ptr - mid - 1);
+    }
+    vector<int> temp;
+    for (int i = l; i <= r; i++)
+        temp.push_back(a[i]);
+    sort(temp.begin(), temp.end());
+    for (int i = l; i <= r; i++)
+        a[i] = temp[i - l];
+    return ans;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << answer(a, 0, n - 1) << endl;
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        //cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
