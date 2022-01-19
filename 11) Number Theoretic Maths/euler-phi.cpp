010bbc5f137f707ed4e1ae69ccfbcc09305eee69
talhaarshad7 <talhaarshad7860@gmail.com>
2022-01-20 00:22:10 +0530

    adding

--- 11) Number Theoretic Maths/euler-phi.cpp
+++ 11) Number Theoretic Maths/euler-phi.cpp
@@ -1,7 +1,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>  
 #include <complex>
 #include <queue>
 #include <set>
@@ -19,67 +19,49 @@
 #include <stack>
 #include <iomanip>
 #include <fstream>
-
+ 
 using namespace std;
-
+ 
 typedef long long ll;
 typedef long double ld;
-typedef pair<int, int> p32;
-typedef pair<ll, ll> p64;
-typedef pair<double, double> pdd;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
 typedef vector<ll> v64;
 typedef vector<int> v32;
-typedef vector<vector<int>> vv32;
-typedef vector<vector<ll>> vv64;
-typedef vector<vector<p64>> vvp64;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
 typedef vector<p64> vp64;
 typedef vector<p32> vp32;
 ll MOD = 998244353;
 ll mod = 1e9 + 7;
 double eps = 1e-12;
-#define forn(i, e) for (ll i = 0; i < e; i++)
-#define forsn(i, s, e) for (ll i = s; i < e; i++)
-#define rforn(i, s) for (ll i = s; i >= 0; i--)
-#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
 #define ln "\n"
-#define dbg(x) cout << #x << " = " << x << ln
+#define dbg(x) cout<<#x<<" = "<<x<<ln
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
 #define INF 2e18
-#define fast_cin()                    \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((ll)(x).size())
+ 
 
-void solve()
-{
-    ll n;
-    cin >> n;
-    set<ll> f;
-    for (ll i = 1; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            f.insert(i);
-            f.insert(n / i);
-        }
-    }
-    for (auto i : f)
-        cout << i << " ";
-    cout << endl;
+void solve(){
 }
 int main()
 {
     fast_cin();
     ll t;
     cin >> t;
-    for (int it = 1; it <= t; it++)
-    {
-        //cout << "Case #" << it+1 << ": ";
+    for(int it=1;it<=t;it++) {
+     //cout << "Case #" << it+1 << ": ";
         solve();
     }
     return 0;
