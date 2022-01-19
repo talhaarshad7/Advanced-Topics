d19699cf544a459cc4afef12937fcb8d61999344
talhaarshad7 <talhaarshad7860@gmail.com>
2022-01-20 01:02:01 +0530

    adding paired cubes

---
+++ 11) Number Theoretic Maths/paired-cubes.cpp
@@ -0,0 +1,106 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+ll mod = 1e9 + 7;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    map<ll, int> m;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        ll norm = 1;
+        ll inorm = 1;
+        for (ll j = 2; j * j <= x; j++)
+        {
+            int cnt = 0;
+            while (x % j == 0)
+            {
+                cnt++;
+                x /= j;
+            }
+            cnt %= 3;
+            for (int k = 0; k < cnt; k++)
+                norm *= j;
+            if (cnt)
+                for (int k = 0; j < 3 - cnt; k++)
+                    inorm *= j;
+        }
+        if (x > 1)
+        {
+            norm *= x;
+            inorm *= x * x;
+        }
+        ans += m[inorm];
+        m[norm]++;
+    }
+    cout << ans << endl;
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        //cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
