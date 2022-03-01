feffe4661f0abc32ba8faf84713a3a0fcc038f96
talhaarshad7 <talhaarshad7860@gmail.com>
2022-03-01 14:12:09 +0530

    adding bricks coloring

---
+++ 15) Dynammic Programming 1/brick-coloring.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+ll mod = 1e9 + 7;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+int dp[2001][2001];
+void solve()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = 0; i < 2001; i++)
+    {
+        for (int j = 0; j < 2001; j++)
+        {
+            dp[i][j] = 0;
+        }
+    }
+    dp[1][0] = m;
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j <= k; j++)
+        {
+            dp[i][j] = dp[i - 1][j];
+            if (j)
+                dp[i][j] = (dp[i][j] + (dp[i - 1][j - 1] * (m - 1)) % MOD) % MOD;
+        }
+    }
+    cout << dp[n][k] << endl;
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    // cin >> t;
+    t = 1;
+    for (int it = 1; it <= t; it++)
+    {
+        // cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
