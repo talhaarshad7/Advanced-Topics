76bfcb19df8a9ac76a066c309b751425af86ce27
Talha Arshad <72298516+talhaarshad7@users.noreply.github.com>
2022-04-29 15:51:18 +0530

    adding easy graph queries

---
+++ 21) Graph Practice Problems/Set 1/easy-graph-queries.cpp
@@ -0,0 +1,219 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+ll mod = 1e9 + 7;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+#define debarr(a, n)            \
+    cout << #a << " : ";        \
+    for (int i = 0; i < n; i++) \
+        cerr << a[i] << " ";    \
+    cerr << endl;
+#define debmat(mat, row, col)         \
+    cout << #mat << " :\n";           \
+    for (int i = 0; i < row; i++)     \
+    {                                 \
+        for (int j = 0; j < col; j++) \
+            cerr << mat[i][j] << " "; \
+        cerr << endl;                 \
+    }
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>
+ostream &operator<<(ostream &os, const pair<S, T> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+ostream &operator<<(ostream &os, const unordered_set<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class S, class T>
+ostream &operator<<(ostream &os, const unordered_map<S, T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class S, class T>
+ostream &operator<<(ostream &os, const map<S, T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+void dbs(string str, T t) { cerr << str << " : " << t << "\n"; }
+template <class T, class... S>
+void dbs(string str, T t, S... s)
+{
+    int idx = str.find(',');
+    cerr << str.substr(0, idx) << " : " << t << ",";
+    dbs(str.substr(idx + 1), s...);
+}
+template <class T>
+void prc(T a, T b)
+{
+    cerr << "[";
+    for (T i = a; i != b; ++i)
+    {
+        if (i != a)
+            cerr << ", ";
+        cerr << *i;
+    }
+    cerr << "]\n";
+}
+
+vector<vector<int>> adj;
+vector<int> vis;
+void dfs(int node, int x)
+{
+    vis[node] = x;
+    for (auto v : adj[node])
+    {
+        if (!vis[v])
+            dfs(v, x);
+    }
+}
+void solve()
+{
+    int n, m, q;
+    cin >> n >> m >> q;
+    adj.resize(n + 1);
+    vis.assign(n + 1, 0);
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    int y = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (vis[i] == 0)
+            dfs(i, y++);
+    }
+    int a[y];
+    // debarr(vis, n + 1);
+    memset(a, 0, sizeof(a));
+    for (int i = 1; i <= n; i++)
+    {
+        a[vis[i]]++;
+    }
+    int xx, yy;
+    // pr(1, q);
+    for (int i = 0; i < q; i++)
+    {
+        int c;
+        cin >> c;
+        // pr(2);
+        switch (c)
+        {
+        case 1:
+            cin >> xx;
+            // pr(xx);
+            cout << a[vis[xx]] << endl;
+            break;
+        case 2:
+            cin >> xx >> yy;
+            if (vis[xx] == vis[yy])
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+            break;
+        }
+    }
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    // cin >> t;
+    for (int it = 1; it <= 1; it++)
+    {
+        // cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
